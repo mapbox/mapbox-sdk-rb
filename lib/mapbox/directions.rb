586e3a180aa736fc107c101c81bbb00c28d5da2c
Sam Fader <sam.fader@mapbox.com>
2018-07-25 15:35:28 -0700

    remove default driving profile to open up other options

--- lib/mapbox/directions.rb
+++ lib/mapbox/directions.rb
@@ -2,7 +2,7 @@ module Mapbox
   class Directions
     include Mapbox::APIOperations::Request
     extend Mapbox::HashUtils
-    def self.directions(waypoints, profile='driving')
+    def self.directions(waypoints, profile)
       formatted_waypoints = waypoints.map {|p| xy_from_hash(p).join ','}.join ';'
       return request(
           :get,
