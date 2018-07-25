a7b9079a9c3ec4ddd0007d380a1f7b105e646f44
Sam Fader <sam.fader@mapbox.com>
2018-07-25 10:30:19 -0700

    update tilequery endpoint to take user input

--- lib/mapbox/tilequery.rb
+++ lib/mapbox/tilequery.rb
@@ -2,10 +2,13 @@ require 'rest-client'
 require 'json'
 
 module Mapbox
-  class TileQuery
+  class Tilequery
     include Mapbox::APIOperations::Request
     extend Mapbox::HashUtils
-    def self.tilequery(mapid = 'mapbox.mapbox-streets-v7', lon='37.80633', lat='-122.42901')
+
+    def self.tilequery(mapid, location)
+      lon = location['longitude'].round(5)
+      lat = location['latitude'].round(5)
       return request(
         :get,
         "/v4/#{mapid}/tilequery/#{lat},#{lon}.json",
