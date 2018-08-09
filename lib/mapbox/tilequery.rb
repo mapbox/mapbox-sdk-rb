25d01d9df7442a83a4a89fb98efac462eb682cfb
Sam Fader <sam.fader@mapbox.com>
2018-08-09 10:10:09 -0700

    add radius and limit as options

--- lib/mapbox/tilequery.rb
+++ lib/mapbox/tilequery.rb
@@ -3,12 +3,12 @@ module Mapbox
     include Mapbox::APIOperations::Request
     extend Mapbox::HashUtils
 
-    def self.tilequery(mapid, location)
+    def self.tilequery(mapid, location, radius, limit)
       lon = location['longitude'].round(5)
       lat = location['latitude'].round(5)
       return request(
         :get,
-        "/v4/#{mapid}/tilequery/#{lat},#{lon}.json",
+        "/v4/#{mapid}/tilequery/#{lon},#{lat}.json?radius=#{radius}&limit=#{limit}",
         nil)
     end
   end
