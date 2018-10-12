c1071465078a4c04020f3f97b2e5179e20bda704
Philippe Legault <plegault@live.ca>
2018-10-12 09:03:41 -0700

    Added parameters to requests for directions  (#28)
    * Added parameters to requests for directions
    
    * Superfluous print statement
    
    * Added explanations for extra parameters to the ruby Directions API

--- README.md
+++ README.md
@@ -32,7 +32,7 @@ placenames = Mapbox::Geocoder.geocode_reverse({
 # Forward geocoding with optional proximity parameter
 places = Mapbox::Geocoder.geocode_forward("Chester, NJ", {:proximity => {:longitude => -74.6968, :latitude => 40.7843}})
 
-# Driving directions with required profile parameter
+# Driving directions with required profile parameter.
 drivingDirections = Mapbox::Directions.directions([{
   "longitude" => -100,
   "latitude" => 38
@@ -40,6 +40,20 @@ drivingDirections = Mapbox::Directions.directions([{
   "longitude" => -90,
   "latitude" => 38
   }], "driving")
+
+# To provide query parameters to the Directions API, such as `geometries`, `language` or `steps`, add those in a Hash as third parameter (find the full list of parameters (here)[https://www.mapbox.com/api-documentation/#retrieve-directions]).
+
+# For instance, to use the `geometries` and `voice_instructions` parameter:
+drivingDirections = Mapbox::Directions.directions([{
+  "longitude" => -100,
+  "latitude" => 38
+}, {
+  "longitude" => -90,
+  "latitude" => 38
+  }], "driving", {
+    geometries: "geojson",
+    voice_instructions: true
+  })
   
 # In the above example, you can substitute `driving` for `driving-traffic`, `cycling` or `walking`. For more, [check out the documentation](https://www.mapbox.com/api-documentation/#directions).
 
--- lib/mapbox/directions.rb
+++ lib/mapbox/directions.rb
@@ -2,11 +2,57 @@ module Mapbox
   class Directions
     include Mapbox::APIOperations::Request
     extend Mapbox::HashUtils
-    def self.directions(waypoints, profile)
+
+    def self.assemble_params(options={})
+      alternatives = options[:alternatives]
+      annotations = options[:annotations]
+      approaches = options[:approaches]
+      banner_instructions = options[:banner_instructions]
+      bearings = options[:bearings]
+      continue_straight = options[:continue_straight]
+      exclude = options[:exclude]
+      geometries = options[:geometries]
+      language = options[:language]
+      overview = options[:overview]
+      radiuses = options[:radiuses]
+      roundabout_exits = options[:roundabout_exits]
+      steps = options[:steps]
+      voice_instructions = options[:voice_instructions]
+      voice_units = options[:voice_units]
+      waypoint_names = options[:waypoint_names]
+      
+      params = ''
+      opts = options.select { |key, value| key != :approaches && key != :bearings && key != :radiuses && key != :waypoint_names  }
+      if opts.length > 0
+        params += "#{params.length > 0 ? '&' : '?'}#{URI.encode_www_form(opts)}"
+      end
+
+      if approaches  then
+        params += "#{params.length > 0 ? '&' : '?'}approaches=#{approaches.join(';')}"
+      end
+
+      if bearings then
+        params += "#{params.length > 0 ? '&' : '?'}bearings=#{bearings.map {|p| bearings.join ','}.join ';'}"
+      end
+
+      if radiuses then
+        params += "#{params.length > 0 ? '&' : '?'}radiuses=#{radiuses.join ';'}"
+      end
+
+      if waypoint_names then
+        params += "#{params.length > 0 ? '&' : '?'}waypoint_names=#{waypoint_names.join ';'}"
+      end
+
+      return params
+    end
+
+    def self.directions(waypoints, profile, options={})
       formatted_waypoints = waypoints.map {|p| xy_from_hash(p).join ','}.join ';'
+      params = self.assemble_params(options)
+      
       return request(
           :get,
-          "/directions/v5/mapbox/#{profile}/#{formatted_waypoints}.json",
+          "/directions/v5/mapbox/#{profile}/#{formatted_waypoints}.json#{params}",
           nil)
     end
   end
