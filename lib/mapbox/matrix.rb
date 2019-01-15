51ad436ba93be035e26f6ae81c222b690f08bbd0
Sam Fader <sam.fader@mapbox.com>
2019-01-14 17:00:09 -0800

    add matrix service

---
+++ lib/mapbox/matrix.rb
@@ -0,0 +1,47 @@
+module Mapbox
+  class Matrix
+    include Mapbox::APIOperations::Request
+    extend Mapbox::HashUtils
+
+    def self.assemble_params(options={})
+      annotations = options[:annotations]
+      approaches = options[:approaches]
+      destinations = options[:destinations]
+      sources = options[:sources]
+      
+      params = ''
+      opts = options.select { |key, value| key!= :annotations && key != :approaches && key != :destinations && key != :sources  }
+      if opts.length > 0
+        params += "#{params.length > 0 ? '&' : '?'}#{URI.encode_www_form(opts)}"
+      end
+
+      if annotations  then
+        params += "#{params.length > 0 ? '&' : '?'}annotations=#{annotations.join(',')}"
+      end
+
+      if approaches  then
+        params += "#{params.length > 0 ? '&' : '?'}approaches=#{approaches.join(';')}"
+      end
+
+      if destinations then
+        params += "#{params.length > 0 ? '&' : '?'}destinations=#{destinations.join(';')}"
+      end
+
+      if sources then
+        params += "#{params.length > 0 ? '&' : '?'}sources=#{sources.join(';')}"
+      end
+
+      return params
+    end
+
+    def self.matrix(waypoints, profile, options={})
+      formatted_waypoints = waypoints.map {|p| xy_from_hash(p).join ','}.join ';'
+      params = self.assemble_params(options)
+      
+      return request(
+          :get,
+          "/directions-matrix/v1/mapbox/#{profile}/#{formatted_waypoints}.json#{params}",
+          nil)
+    end
+  end
+end
