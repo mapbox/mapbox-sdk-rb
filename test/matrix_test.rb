2879a092e287751d5d16f031d9b4e4a27a0b4deb
Sam Fader <sam.fader@mapbox.com>
2019-01-16 10:43:20 -0800

    update test names, add additional test

--- test/matrix_test.rb
+++ test/matrix_test.rb
@@ -5,7 +5,21 @@ require 'shoulda'
 
 module Mapbox
   class MatrixTest < Test::Unit::TestCase
-    should "#matrix" do
+    should "#matrix with just coordinates" do
+      Mapbox.access_token = ENV["MapboxAccessToken"]
+      result = Mapbox::Matrix.matrix([{
+        "longitude" => -122.42,
+        "latitude" => 37.78
+      }, {
+        "longitude" => -122.45,
+        "latitude" => 37.91
+      }, {
+       "longitude" => -122.48,
+        "latitude" => 37.73
+        }], "driving")
+      assert result
+    end
+    should "#matrix with optional params" do
       Mapbox.access_token = ENV["MapboxAccessToken"]
       result = Mapbox::Matrix.matrix([{
         "longitude" => -122.42,
