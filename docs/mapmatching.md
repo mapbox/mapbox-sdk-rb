a95d616db6c08d46bb71b60f6b1c533c419715ca
Sam Fader <sam.fader@mapbox.com>
2019-01-16 12:21:01 -0800

    add map matching

---
+++ docs/mapmatching.md
@@ -0,0 +1,43 @@
+# Map Matching
+
+```rb
+require "mapbox-sdk"
+Mapbox.access_token = "YOUR_ACCESS_TOKEN"
+
+# Driving directions with required profile parameter.
+matching = Mapbox::MapMatching.map_matching([{
+  "longitude" => -117.17282,
+  "latitude" => 32.71204
+}, {
+  "longitude" => -117.17288,
+  "latitude" => 32.71225
+}, {
+  "longitude" => -117.17293,
+  "latitude" => 32.71244
+  },{
+  "longitude" => -117.17292,
+  "latitude" => 32.71256
+  }], "driving")
+
+# To provide query parameters to the Matrix API, such as `annotations`, `approaches` or `sources`, add those in a Hash as third parameter (find the full list of parameters (here)[https://www.mapbox.com/api-documentation/navigation/#matrix]).
+
+# For instance, to use the `geometries` and `voice_instructions` parameter:
+matching = Mapbox::MapMatching.map_matching([{
+  "longitude" => -117.17282,
+  "latitude" => 32.71204
+}, {
+  "longitude" => -117.17288,
+  "latitude" => 32.71225
+}, {
+  "longitude" => -117.17293,
+  "latitude" => 32.71244
+  },{
+  "longitude" => -117.17292,
+  "latitude" => 32.71256
+  }], "driving", {
+    annotations: ["duration", "distance", "speed"],
+    approaches: ["curb","curb","curb","curb"]
+  })
+
+# In the above example, you can substitute `driving` for `driving-traffic`, `cycling` or `walking`. For more, [check out the documentation](https://www.mapbox.com/api-documentation/navigation/#matrix).
+```
---
+++ lib/mapbox/map_matching.rb
@@ -0,0 +1,62 @@
+module Mapbox
+  class MapMatching
+    include Mapbox::APIOperations::Request
+    extend Mapbox::HashUtils
+
+    def self.assemble_params(options={})
+      annotations = options[:annotations]
+      approaches = options[:approaches]
+      geometries = options[:geometries]
+      language = options[:language]
+      overview = options[:overview]
+      radiuses = options[:radiuses]
+      steps = options[:steps]
+      tidy = options[:tidy]
+      timestamps = options[:timestamps]
+      waypoint_names = options[:waypoint_names]
+      waypoints = options[:waypoints]
+      
+      params = ''
+      opts = options.select { |key, value| key != :annotations && key != :approaches && key != :radiuses && key != :timestamps && key != :waypoint_names && key != :waypoints  }
+      if opts.length > 0
+        params += "#{params.length > 0 ? '&' : '?'}#{URI.encode_www_form(opts)}"
+      end
+
+      if annotations  then
+        params += "#{params.length > 0 ? '&' : '?'}annotations=#{annotations.join(',')}"
+      end
+
+      if approaches  then
+        params += "#{params.length > 0 ? '&' : '?'}approaches=#{approaches.join(';')}"
+      end
+
+      if radiuses then
+        params += "#{params.length > 0 ? '&' : '?'}radiuses=#{radiuses.join ';'}"
+      end
+
+      if timestamps then
+        params += "#{params.length > 0 ? '&' : '?'}timestamps=#{timestamps.join ';'}"
+      end
+
+      if waypoint_names then
+        params += "#{params.length > 0 ? '&' : '?'}waypoint_names=#{waypoint_names.join ';'}"
+      end
+
+      if waypoints then
+        params += "#{params.length > 0 ? '&' : '?'}waypoints=#{waypoints.join ';'}"
+      end
+
+      return params
+    end
+
+    def self.map_matching(waypoints, profile, options={})
+      formatted_waypoints = waypoints.map {|p| xy_from_hash(p).join ','}.join ';'
+      params = self.assemble_params(options)
+      
+      return request(
+          :get,
+          "/matching/v5/mapbox/#{profile}/#{formatted_waypoints}.json#{params}",
+          nil)
+    end
+  end
+end
