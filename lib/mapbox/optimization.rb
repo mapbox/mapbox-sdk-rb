45ca162438f842ccc0df1b024d8447747ad45854
Sam Fader <sam.fader@mapbox.com>
2019-01-24 16:20:14 -0800

    don't need to do anything special to bearings param, removed

--- lib/mapbox/optimization.rb
+++ lib/mapbox/optimization.rb
@@ -31,11 +31,6 @@ module Mapbox
         params += "#{params.length > 0 ? '&' : '?'}approaches=#{approaches.join(';')}"
       end
 
-      # this one is weird - Takes 2 values per waypoint: an angle clockwise from true north between 0 and 360, and the range of degrees by which the angle can deviate (recommended value is 45° or 90°), formatted as {angle, degrees}. If provided, the list of bearings must be the same length as the list of waypoints. However, you can skip a coordinate and show its position in the list with the ; separator.
-      if bearings  then
-        params += "#{params.length > 0 ? '&' : '?'}bearings=#{bearings.join(';')}"
-      end
-
       if distributions  then
         params += "#{params.length > 0 ? '&' : '?'}distributions=#{distributions.join(';')}"
       end
@@ -44,23 +39,11 @@ module Mapbox
         params += "#{params.length > 0 ? '&' : '?'}radiuses=#{radiuses.join(';')}"
       end
 
-      if timestamps then
-        params += "#{params.length > 0 ? '&' : '?'}timestamps=#{timestamps.join(';')}"
-      end
-
-      if waypoint_names then
-        params += "#{params.length > 0 ? '&' : '?'}waypoint_names=#{waypoint_names.join(';')}"
-      end
-
-      if waypoints then
-        params += "#{params.length > 0 ? '&' : '?'}waypoints=#{waypoints.join(';')}"
-      end
-
       return params
     end
 
-    def self.map_matching(waypoints, profile, options={})
-      formatted_waypoints = waypoints.map {|p| wxy_from_hash(p).join ','}.join ';'
+    def self.optimization(waypoints, profile, options={})
+      formatted_waypoints = waypoints.map {|p| xy_from_hash(p).join ','}.join ';'
       params = self.assemble_params(options)
       
       return request(
