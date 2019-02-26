4d5d1f944ad96f4c7433ff1457a1b029b3f0f445
Sam Fader <sam.fader@mapbox.com>
2019-02-25 16:40:57 -0800

    update matrix endpoint to be more compact

--- lib/mapbox/matrix.rb
+++ lib/mapbox/matrix.rb
@@ -3,43 +3,30 @@ module Mapbox
     include Mapbox::APIOperations::Request
     extend Mapbox::HashUtils
 
-    def self.assemble_params(options={})
-      annotations = options[:annotations]
-      approaches = options[:approaches]
-      if options[:destinations].kind_of?(Array)
-        destinations = options[:destinations]
-      end
-      sources = options[:sources]
-      
-      params = ''
-      opts = options.select { |key, value| key!= :annotations && key != :approaches && key != :destinations && key != :sources  }
-      if opts.length > 0
-        params += "#{params.length > 0 ? '&' : '?'}#{URI.encode_www_form(opts)}"
-      end
+    def self.assemble_params(opts={})
+      self.joinArrayParam(opts, :destinations)
+      self.joinArrayParam(opts, :annotations)
+      self.joinArrayParam(opts, :approaches)
+      self.joinArrayParam(opts, :sources)
 
-      if annotations  then
-        params += "#{params.length > 0 ? '&' : '?'}annotations=#{annotations.join(',')}"
-      end
+      return "?#{URI.encode_www_form(opts)}"
+    end
 
-      if approaches  then
-        params += "#{params.length > 0 ? '&' : '?'}approaches=#{approaches.join(';')}"
+    def self.joinArrayParam(opts, name)
+      if opts[name].kind_of?(Array)
+        # The annotations parameter requires a comma separated list
+        if name == :annotations
+          opts[name] = opts[name].join(',')
+        # Other parameters must be be semi-colon separated
+        else
+          opts[name] = opts[name].join(';')
+        end
       end
-
-      if destinations then
-        params += "#{params.length > 0 ? '&' : '?'}destinations=#{destinations.join(';')}"
-      end
-
-      if sources then
-        params += "#{params.length > 0 ? '&' : '?'}sources=#{sources.join(';')}"
-      end
-
-      return params
     end
 
     def self.matrix(waypoints, profile, options={})
       formatted_waypoints = waypoints.map {|p| xy_from_hash(p).join ','}.join ';'
       params = self.assemble_params(options)
-      
       return request(
           :get,
           "/directions-matrix/v1/mapbox/#{profile}/#{formatted_waypoints}.json#{params}",
